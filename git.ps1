0bc97c6ca460292da54ef73bbdc79e0fb8c30dde
Frédéric <98284200+2Fred1Jacques@users.noreply.github.com>
2024-01-08 15:08:15 +0100

    save 2024-01-08 15:08:15

--- git.ps1
+++ git.ps1
@@ -1,45 +1,26 @@
-# Ajouter tous les fichiers modifiés à l'index
-git add .
-
-# Faire un commit avec un message contenant la date et l'heure actuelle
-$commit_message = "save $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
-git commit -m $commit_message
-
-# Pousser les modifications vers le dépôt distant
-git push
-
-# Retourner la date et l'heure du commit avec un affichage plus lisible dans la console PowerShell (### le message ###)
-
-# Exemple de sortie dans la console :
-# ###########################
-# ### 2020-04-01 14:00:00 ###
-# ###########################
-
-# cas nothing to commit, working tree clean
-
+# Vérifier si le dépôt a des modifications non commitées
 if (git status | Select-String -Pattern "nothing to commit, working tree clean") {
-    Write-Host "###############################################################################################################" -ForegroundColor Cyan
-    Write-Host "   Aucun changement détecté, rien à commiter !" -ForegroundColor Yellow
-    Write-Host "###############################################################################################################" -ForegroundColor Cyan
-    exit
+    Write-Host "Aucun changement détecté, rien à commiter !" -ForegroundColor Yellow
 } else {
-# Obtenir la date et l'heure du dernier commit dans un format standard ISO 8601
-$commitDateRaw = git log -1 --format=%cI
+    # Ajouter tous les fichiers modifiés à l'index
+    git add .
 
-# Convertir la chaîne de date en un objet DateTime et le formater pour l'affichage
-$commitDateFormatted = Get-Date $commitDateRaw -Format "dddd d MMMM yyyy, HH:mm:ss"
+    # Faire un commit avec un message contenant la date et l'heure actuelle
+    $commit_message = "save $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
+    git commit -m $commit_message
 
-# Obtenir l'URL du dépôt distant
-$remoteUrl = git remote -v | Select-String -Pattern "origin\s+(?<url>git@github\.com:(.+)\.git)" | ForEach-Object { $_.Matches[0].Groups["url"].Value }
+    # Pousser les modifications vers le dépôt distant
+    git push
 
-# Extraire le nom du compte et le nom du dépôt
-$repoDetails = $remoteUrl -replace 'git@github.com:', ''
-$accountName, $repoName = $repoDetails.Split('/')[0..1]
+    # Extraire les informations du dernier commit
+    $commitDateRaw = git log -1 --format=%cI
+    $commitDateFormatted = Get-Date $commitDateRaw -Format "dddd d MMMM yyyy, HH:mm:ss"
+    $remoteUrl = git remote -v | Select-String -Pattern "origin\s+(?<url>git@github\.com:(.+)\.git)" | ForEach-Object { $_.Matches[0].Groups["url"].Value }
+    $repoDetails = $remoteUrl -replace 'git@github.com:', ''
+    $accountName, $repoName = $repoDetails.Split('/')[0..1]
 
-# Afficher la date et l'heure du commit dans un format lisible dans la console PowerShell avec des couleurs
-Write-Host "###############################################################################################################" -ForegroundColor Cyan
-Write-Host "   Date du commit : $commitDateFormatted" -ForegroundColor Yellow
-Write-Host "   Dépôt : $accountName / $repoName" -ForegroundColor Yellow
-Write-Host "   Commit et push terminé !" -ForegroundColor Green
-Write-Host "###############################################################################################################" -ForegroundColor Cyan
+    # Afficher les informations du commit
+    Write-Host "Date du commit : $commitDateFormatted" -ForegroundColor Yellow
+    Write-Host "Dépôt : $accountName / $repoName" -ForegroundColor Yellow
+    Write-Host "Commit et push terminé !" -ForegroundColor Green
 }
