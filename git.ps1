443bab3f530a0f36431059218b72d2a21cbfa536
Frédéric <98284200+2Fred1Jacques@users.noreply.github.com>
2024-01-08 12:00:05 +0100

    save 2024-01-08 12:00:05

--- git.ps1
+++ git.ps1
@@ -8,5 +8,14 @@ git commit -m $commit_message
 # Pousser les modifications vers le dépôt distant
 git push
 
-# Retourner la date et l'heure du commit
-$commit_message
+# Retourner la date et l'heure du commit avec un affichage plus lisible dans la console PowerShell (### le message ###)
+
+# Exemple de sortie dans la console :
+# ###########################
+# ### 2020-04-01 14:00:00 ###
+# ###########################
+
+echo "#############################"
+echo "### $commitDate ###"
+echo "#############################"
+
